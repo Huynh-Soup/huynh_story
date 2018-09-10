f90c156f7c1e86d81a40e29cb213821d2134c859
David Huynh <davidhuy.71461@redlandsschools.net>
2018-09-10 15:25:31 +0000

    boi

---
+++ second.py
@@ -0,0 +1,10 @@
+"""
+
+Write a program that will ask the user what thier age is and then
+determine if they are old enough to vote or not and respond appropriatlely
+
+"""
+
+age = int(input("Hello, how old are you?: "))
+
+if age = 17 
