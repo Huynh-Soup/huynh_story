c44153a21c210924660b9674db3a25568bc86553
David Huynh <davidhuy.71461@redlandsschools.net>
2018-09-19 14:47:36 +0000

    boi u boi

--- Huynh_Pythagorean/Huynh_Pythagorean.py
+++ Huynh_Pythagorean/Huynh_Pythagorean.py
@@ -3,7 +3,7 @@ Write a program that will ask the user to
 enter their name and respond with a greeting using this name.
 """
 name = input("Hello what is your name: ")
-print("Hello " + str(name) + " Welcome to the Pythagorean simulator, we will now find")
+print("Hello " + str(name) + " Welcome to the Pythagorean simulator, we will now find the value of C ")
 
 a = int(input("Enter a value for a: "))
 b = int(input("Enter a value for b: "))
